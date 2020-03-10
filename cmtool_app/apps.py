d8e6315eb1d1ed5045c874a504481324c384718c
Iverson <kunpeng.zhao@163.com>
2020-03-10 23:31:37 +0800

    update to restapiframework

--- cmtool_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CmtoolAppConfig(AppConfig):
-    name = 'cmtool_app'
--- cmtools/asgi.py
+++ cmtools/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for muyu_cmtool project.
+ASGI config for cmtools project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muyu_cmtool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cmtools.settings')
 
 application = get_asgi_application()
