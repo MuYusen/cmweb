59513e4f1b1588d9e4e0374321e5f79396805d1f
Iverson <kunpeng.zhao@163.com>
2020-05-13 22:17:11 +0800

    add blog app
    Change-Id: I9150d012a57d362ee86a172b8ae6e6e5a62ed6b9

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
  
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES=(
         ('draft','Draft'),
@@ -23,3 +29,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()    
+
+    def get_absolute_url(self):
+        return  reverse('blog:post_detail',args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
